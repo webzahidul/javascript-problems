5536ae8be8812ba770ebb9d5b4102d80d63f4d15
Web Zahidul <webzahidul@gmail.com>
2021-08-08 16:59:16 +0600

    max-done

---
+++ max.js
@@ -0,0 +1,21 @@
+var business = 450;
+var minister = 650;
+var sochib = 770;
+
+if(business > minister){
+    if(business > sochib){
+        console.log("business is bigger"); 
+    }
+    else{
+        console.log("sochib is bigger"); 
+    }
+}
+else{
+    if(minister > sochib){
+        console.log("minister is bigger");
+    }
+    else{
+        console.log("sochib is bigger");
+    }
+}
+ 
