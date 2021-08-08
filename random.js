72b14fc267a827df7a6a6bb5dca3205c44d322f9
Web Zahidul <webzahidul@gmail.com>
2021-08-08 07:39:22 +0600

    random-done

---
+++ random.js
@@ -0,0 +1,9 @@
+var num = 2.2522;
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+for(i = 0; i <10; i++){
+var randomNum = Math.random() * 10;
+var output = Math.round(randomNum);
+console.log(output);  
+}
