a914cfc4098fc511987659d58bc06c2f52a03703
Web Zahidul <webzahidul@gmail.com>
2021-08-11 10:52:54 +0600

    duplicate-done

---
+++ duplicate.js
@@ -0,0 +1,11 @@
+var name = [1, 5, 8, 4, 1, 4, 5];
+var uniqueName = [];
+
+for(var i = 0; i <name.length; i++) {
+    var element = name[i];
+    var index = uniqueName.indexOf(element);
+    if(index == -1){
+        uniqueName.push(element);
+    }
+}
+console.log(uniqueName);
