1bef9c0f3ef48e9571086f83fdba0891a7214485
Web Zahidul <webzahidul@gmail.com>
2021-08-15 01:32:09 +0600

    factorial-recursive-done

---
+++ factorialRecursive.js
@@ -0,0 +1,13 @@
+function factorialRecursive(num){
+    if(num == 1){
+        console.log(num);
+        return 1;
+    }
+    else{
+        console.log(num, num-1);
+        return num * factorialRecursive(num - 1);
+    }
+}
+
+var result = factorialRecursive(5);
+console.log(result);
